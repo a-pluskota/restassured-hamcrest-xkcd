12e7f25937d6fdf16bf26650be1c4f39f75cd292
Pluskota Anna <anna.pluskota@totalmoney.pl>
2019-11-24 23:22:53 +0100

    version 1.0-SNAPSHOT

--- src/test/java/com/xkcd/testCases/XkcdLatestWebcomicTest.java
+++ src/test/java/com/xkcd/testCases/XkcdLatestWebcomicTest.java
@@ -1,19 +1,7 @@
 package com.xkcd.testCases;
 
-import com.xkcd.XkcdResponseParametersTests;
 import com.xkcd.XkcdWebcomicImageResponseParametersTests;
-
-import java.util.Random;
 
 public class XkcdLatestWebcomicTest extends XkcdWebcomicImageResponseParametersTests {
 
-    private static final int randomWebcomicID = new Random()
-            .nextInt((new XkcdResponseParametersTests()
-                    .validateXkcdResponseJsonModel()
-                    .getNum() - 117) + 117);
-
-    public XkcdLatestWebcomicTest() {
-
-        super(randomWebcomicID);
-    }
 }
--- src/test/java/com/xkcd/testCases/XkcdRandomWebcomicTest.java
+++ src/test/java/com/xkcd/testCases/XkcdRandomWebcomicTest.java
@@ -1,8 +1,19 @@
 package com.xkcd.testCases;
 
+import com.xkcd.XkcdResponseParametersTests;
 import com.xkcd.XkcdWebcomicImageResponseParametersTests;
+
+import java.util.Random;
 
 public class XkcdRandomWebcomicTest extends XkcdWebcomicImageResponseParametersTests {
 
+    private static final int randomWebcomicID = new Random()
+            .nextInt((new XkcdResponseParametersTests()
+                    .validateXkcdResponseJsonModel()
+                    .getNum() - 117) + 117);
 
+    public XkcdRandomWebcomicTest() {
+
+        super(randomWebcomicID);
+    }
 }
